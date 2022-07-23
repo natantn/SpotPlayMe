120c2e7210ca401d5a9d1590ab41cd7bc2aa77fe
Natan Figueiredo <natanfigueiredo27@gmail.com>
2022-07-22 22:21:13 -0300

    adding comments to sync controller

--- controllers/spotify.go
+++ controllers/spotify.go
@@ -21,41 +21,43 @@ func SyncPlaylists(c *gin.Context) {
 		Username:    username,
 		AccessToken: spotify.GenerateToken(),
 	}
-
 	spotContext.FetchPlaylistsFromUser()
 
 	DatabasePlaylists := []*models.Playlist{}
-
 	for _, playlistFetched := range spotContext.PlaylistsFetched {
 
-		playlistMusics := []models.Music{}
+		//Fetching each music in playlist
+		playlistMusics := []models.Music{}		
 		for _, track := range playlistFetched.Tracks.Items {
 			music := models.Music{}
 			database.DB.Where(&models.Music{SpotifyID: track.Track.ID}).FirstOrCreate(&music)
 			hasChanged := music.FillFetchedMusic(&track.Track)
-			if hasChanged {	database.DB.Save(&music) }
-
+			if hasChanged {
+				database.DB.Save(&music)
+			}
 			playlistMusics = append(playlistMusics, music)
 		}
 
+		//Fetching playlist
 		playlist := models.Playlist{}
 		database.DB.Where(&models.Playlist{SpotifyID: playlistFetched.ID}).FirstOrCreate(&playlist)
 		hasChanged := playlist.FillFetchedPlaylist(playlistFetched, &playlistMusics)
-		if hasChanged {	database.DB.Save(&playlist) }
+		if hasChanged {
+			database.DB.Save(&playlist)
+		}
 
+		//Updating musics sequence
 		for seq, music := range playlist.Musics {
 			trackInPlaylist := models.PlaylistMusics{
 				PlaylistID: int(playlist.ID),
 				MusicID:    int(music.ID),
 			}
 			database.DB.First(&trackInPlaylist)
-			if trackInPlaylist.ReprodutionSequence != seq + 1 {
+			if trackInPlaylist.ReprodutionSequence != seq+1 {
 				trackInPlaylist.SetMusicSequenceInPlaylist(seq + 1)
 				database.DB.Save(&trackInPlaylist)
 			}
-
 		}
-
 		DatabasePlaylists = append(DatabasePlaylists, &playlist)
 	}
 
